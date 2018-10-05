047338e67c80a0099cf1a659472f3dfe01a56215
Ben Warburton <ben@travoltron.com>
2018-10-05 00:45:14 -0400

    Adding/removing valid classes and validation on all email inputs

--- the_matriarchy_landing_page/scripts/main.js
+++ the_matriarchy_landing_page/scripts/main.js
@@ -1,2 +1,17 @@
+$(document).ready(function () {
+    $('.required').prop('required', true);
+    $(".valid-email").blur(function() {
+        if (isEmail($(this).val())) {
+            $(this).addClass("valid-input");
+        }
+        if (!isEmail($(this).val())) {
+            $(this).removeClass("valid-input");
+        }
+    });
 
+});
 
+function isEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+}
